c8d04b0b687c42273817aa7265e95e597e50aa47
Ernie <ernie@earthtoernie.com>
2022-02-06 15:51:42 -0500

    get to pio_counter_milestone_0

--- pico_display/demo.cpp
+++ pico_display/demo.cpp
@@ -13,6 +13,8 @@
 
 #include "count.pio.h"
 
+#include "pico/stdlib.h"
+
 #define INPUT_PIN 5
 #define GATE_PIN 4
 #define PULSE_FIN_PIN 3
@@ -20,8 +22,8 @@
 uint32_t max_count = 4294967295;// const((1 << 32) - 1), highest unsigned int
 
 
-PIO sd_pio = pio1;
-bool update_flag = false;
+PIO sd_pio = pio0;
+volatile bool update_flag = false;
 uint16_t data [2] = { 0, 0 };
 
 void init_sm(uint16_t freq) {
@@ -37,46 +39,54 @@ void init_sm(uint16_t freq) {
 
     // starts state machines.
     uint offset_gate_program = pio_add_program(sd_pio, &gate_program);
-    uint offset_clock_count_program = pio_add_program(sd_pio, &clock_count_program);
-    uint offset_pulse_count_program = pio_add_program(sd_pio, &pulse_count_program);
+//    uint offset_clock_count_program = pio_add_program(sd_pio, &clock_count_program);
+//    uint offset_pulse_count_program = pio_add_program(sd_pio, &pulse_count_program);
 
     gate_program_init(sd_pio, 0, offset_gate_program);
     pio_sm_put(sd_pio, 0, freq);
     pio_sm_exec(sd_pio, 0, pio_encode_pull(false, false));
 
-    clock_count_program_init(sd_pio, 1, offset_clock_count_program);
-    pio_sm_put(sd_pio, 1, max_count);
-    pio_sm_exec(sd_pio, 1, pio_encode_pull(false, false));
+//    clock_count_program_init(sd_pio, 1, offset_clock_count_program);
+//    pio_sm_put(sd_pio, 1, max_count);
+//    pio_sm_exec(sd_pio, 1, pio_encode_pull(false, false));
 
-    pulse_count_program_init(sd_pio, 2, offset_pulse_count_program);
-    pio_sm_put(sd_pio, 2, max_count-1);
-    pio_sm_exec(sd_pio, 2, pio_encode_pull(false, false));
+//    pulse_count_program_init(sd_pio, 2, offset_pulse_count_program);
+//    pio_sm_put(sd_pio, 2, max_count-1);
+//    pio_sm_exec(sd_pio, 2, pio_encode_pull(false, false));
 
-    pio_sm_set_enabled(sd_pio, 0, true);
     pio_sm_set_enabled(sd_pio, 1, false);
     pio_sm_set_enabled(sd_pio, 2, false);
+    pio_sm_set_enabled(sd_pio, 0, true);
+    pio_set_irq0_source_enabled(sd_pio, pis_interrupt0, true);
+
 
 }
 
 //https://raspberrypi.github.io/pico-sdk-doxygen/group__hardware__irq.html
 void isr()
 {
+    pio_interrupt_clear(pio0, 0);
+    irq_clear(PIO0_IRQ_0);
 
     printf("IRQ\n");
+    pio_sm_put(sd_pio, 0, 125000);
+    pio_sm_exec(sd_pio, 0, pio_encode_pull(false, false));
     if(!update_flag){
-        pio_sm_put(sd_pio, 0, 12500);
-        pio_sm_exec(sd_pio, 0, pio_encode_pull(false, false));
-        data[0] = pio_sm_get(sd_pio, 1);
-        data[1] = pio_sm_get(sd_pio, 2);
+        printf("irq");
+
+        //data[0] = pio_sm_get(sd_pio, 1);
+        //data[1] = pio_sm_get(sd_pio, 2);
         update_flag = true;
     }
-    pio_interrupt_clear(pio0, 0);
+//    pio_interrupt_clear(pio0, 0);
 //    pio_interrupt_clear(pio0, 0);
 //    irq_clear(PIO0_IRQ_0);
 }
 
 //void count_handler(sm)
 int main(){
+    // minicom -D /dev/ttyACM0 -b 115200
+    // https://forums.raspberrypi.com/viewtopic.php?t=316677
     stdio_init_all();
 
 
@@ -88,14 +98,20 @@ int main(){
     pio_set_irq0_source_enabled(sd_pio, pis_interrupt0, true);
 
     uint32_t i;
+    uint32_t helloCount = 1;
     while(true){
-        uint32_t clock_count = 2*(max_count - data[0]+1);
-        uint32_t pulse_count = max_count - data[1];
-        float frequency = pulse_count * (125000208.6 / clock_count);
-        printf("%d\n", i);
-        printf("Clock count: %u\n", clock_count);
-        printf("Input count: %u\n", pulse_count);
-        printf("Frequency: %f", frequency);
+        if(update_flag == true) {
+            printf("foo %u\n", helloCount++);
+            update_flag = false;
+        }
+        //sleep_ms(1000);
+        //uint32_t clock_count = 2*(max_count - data[0]+1);
+        //uint32_t pulse_count = max_count - data[1];
+        //float frequency = pulse_count * (125000208.6 / clock_count);
+        //printf("%d\n", i);
+        //printf("Clock count: %u\n", clock_count);
+        //printf("Input count: %u\n", pulse_count);
+        //printf("Frequency: %f", frequency);
     }
 
 
