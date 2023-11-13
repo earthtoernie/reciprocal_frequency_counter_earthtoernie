fcb124f7873e30fbb404af42a93bdca5f3cacd32
Ernie <ernie@earthtoernie.com>
2023-11-12 20:47:45 -0500

    update squaregen boilerplate

--- squaregen/main.c
+++ squaregen/main.c
@@ -4,6 +4,8 @@
 const uint LED_PIN = 25;
 
 int main() {
+    //gpio_set_function(16, GPIO_FUNC_UART); //TX
+    //gpio_set_function(17, GPIO_FUNC_UART); //RX
     stdio_init_all();
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
